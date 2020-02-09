ff643071c71fee8d27d754b94dabedace7705934
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-02-09 16:33:53 +0530

    parse url done

--- core/index.js
+++ core/index.js
@@ -1,18 +1,25 @@
-//1. this is how most of javascript codes are executed which are present 
+//1. this is how most of javascript codes are executed which are present
 // in a file or in entire project.
-console.log('hello World');
+console.log("hello World");
 
 //4. Read file(theory.md) here using fs module and console the results of operation.
-
+const fs = require("fs");
+fs.readFile("theory.md", (err, file) => {
+	console.log(err, file);
+});
 //6. Blocking code
- // Run it first and observe the output
-//  var file = fs.readfileSync('./theory.md');
-//  console.log(file);
-//  console.log('run me first');
- 
- // change it to run it in non-blocking manner.
- // Observe the difference in blocking and non-blocking code. 
+// Run it first and observe the output
+var file = fs.readfileSync("./theory.md", (err, file) => {
+	console.log(file);
+});
+console.log("run me first");
+
+// change it to run it in non-blocking manner.
+var file = fs.readFileSync("./theory.md");
+console.log(file);
+console.log("run me first");
+
+// Observe the difference in blocking and non-blocking code.
 
 // 10. require math.js
-  // console const pie and add and multiply function.
-
+// console const pie and add and multiply function.
